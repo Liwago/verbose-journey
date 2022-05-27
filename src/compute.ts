88d4f71d55969e51adea14cbd1e31431cbe5a9d0
liwago <liam.goethals@wegroup.be>
2022-05-27 20:49:47 +0200

    Create socre calculator for given input

--- src/compute.ts
+++ src/compute.ts
@@ -1,5 +1,41 @@
-import { Game } from "./types";
+import {Game, Frame, LastFrame} from "./types";
 
 export function compute(game: Game): number {
-  throw new Error("Not yet implemented"); // TODO
+  let score = 0
+
+  function sum(array: Array<number>) {
+    return array.reduce((acc: number, obj: number) => (acc + obj))
+  }
+
+  const isStrike = (frame: Frame | LastFrame): boolean => (frame[0] == 10);
+  const isSpare = (frame: Frame | LastFrame): boolean => ((sum(frame)) == 10 && (frame[0] != 10));
+
+  for (let indexStr in game) {
+    let index = Number(indexStr)
+    const prevprev = game[index - 2] ? game[index - 2] : ([0, 0] as Frame)
+    const prev = game[index - 1] ? game[index - 1] : ([0, 0] as Frame)
+    const curr = game[index]
+    const next = game[index + 1]
+
+    score = score + sum(curr);
+
+    if (isStrike(prev)) {
+      score = score + sum(curr.slice(0, 2));
+      if (next) {
+        if (isStrike(curr)) {
+          if (isStrike(next)) {
+            score = score + 10
+          } else if (prevprev && isStrike(prevprev)) {
+            score = score + next.slice(0, 2)[0];
+          }
+        }
+      } else if (isStrike(prev) && isStrike(prevprev) && !(isStrike(curr))) {
+        score = score + curr.slice(0, 2)[0];
+      }
+    }
+    if (isSpare(prev)) {
+      score = score + curr[0];
+    }
+  }
+  return score
 }
